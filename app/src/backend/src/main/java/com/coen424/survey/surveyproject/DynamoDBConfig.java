b86b9a8e08c0e0d7043d95436c3318869113e8c8
Mher Charachian <22100551+MIst3rM@users.noreply.github.com>
2022-09-29 10:23:00 -0400

    added dynamodb configuration

--- app/src/backend/src/main/java/com/coen424/survey/surveyproject/DynamoDBConfig.java
+++ app/src/backend/src/main/java/com/coen424/survey/surveyproject/DynamoDBConfig.java
@@ -1,43 +1,32 @@
 package com.coen424.survey.surveyproject;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-@EnableDynamoDBRepositories
-        (basePackages = "com.baeldung.spring.data.dynamodb.repositories")
 public class DynamoDBConfig {
 
     @Value("${amazon.dynamodb.endpoint}")
     private String amazonDynamoDBEndpoint;
 
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
-
-    @Value("${AWS_REGION}")
+    @Value("${amazon.aws.region}")
     private String amazonAWSRegion;
 
-    private AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, amazonAWSRegion)).build();
-
-    private AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(
-                amazonAWSAccessKey, amazonAWSSecretKey);
+    private AmazonDynamoDB amazonDynamoDB(){
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, amazonAWSRegion))
+                .build();
     }
-
-    private DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDB);
-
-    public DynamoDBMapper getMapper() {
-        return mapper;
+    @Bean
+    public DynamoDBMapper mapper() {
+        return new DynamoDBMapper(amazonDynamoDB());
     }
 }
--- app/src/backend/src/main/java/com/coen424/survey/surveyproject/SurveyprojectApplication.java
+++ app/src/backend/src/main/java/com/coen424/survey/surveyproject/SurveyprojectApplication.java
@@ -10,14 +10,6 @@ public class SurveyprojectApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SurveyprojectApplication.class, args);
-
-//		try (DynamoDbClient client = DynamoDbClient.builder()
-//				.region(Region.US_EAST_1)
-//				.endpointOverride(
-//						URI.create("http://dynamodb-local:8000")
-//				).build()) {
-//			System.out.println("DynamoDB local started");
-//		}
 	}
 
 }
